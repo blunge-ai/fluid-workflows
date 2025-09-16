4785e52884c7aeb16906b13720b159df13f28d87
ryuuseijin <ryuuseijin@fastmail.com>
2025-09-16 16:54:38 +0800

    helpers

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,25 @@ export {
  JobQueueWorkflowDispatcher
 } from './JobQueueWorkflowDispatcher';
 export { Config } from './Config';
+
+import { Config } from './Config';
+import { JobQueueWorkflowRunner } from './JobQueueWorkflowRunner';
+import { JobQueueWorkflowDispatcher } from './JobQueueWorkflowDispatcher';
+import { Workflow } from './Workflow';
+import type { JobQueueEngine } from './JobQueueEngine';
+import type { Logger } from './utils';
+import type { WfArray, NamesOfWfs, RequireKeys } from './typeHelpers';
+
+export function config<const Wfs extends WfArray<string>, const Qs extends Record<NamesOfWfs<Wfs>, string>>(args: {
+  engine: JobQueueEngine,
+  workflows: Wfs,
+  queues: RequireKeys<Qs, NamesOfWfs<Wfs>>,
+  logger?: Logger,
+}) {
+  const cfg = new Config<NamesOfWfs<Wfs>, Wfs, Qs>(args);
+  const runner = new JobQueueWorkflowRunner(cfg);
+  const dispatcher = new JobQueueWorkflowDispatcher(cfg);
+  return { runner, dispatcher } as const;
+}
+
+export const workflow: typeof Workflow.create = Workflow.create;
